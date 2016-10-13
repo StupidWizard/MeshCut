d5e00be1740d596076a90f2eb9188f316be462e7
thantieuhodo91 <toinguyenkhac@gmail.com>
2016-10-13 17:00:19 +0900

    use joint Plane to check left and right part can joint after cut

---
+++ MeshCut/Assets/Scripts/JointPlane.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JointPlane : MonoBehaviour {
+
+	public bool beCut = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
